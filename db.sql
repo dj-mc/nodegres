ebb56e979dd38c6a3a93c8d7dec6740cc366f4c6
dj-mc <49037253+dj-mc@users.noreply.github.com>
2022-11-12 00:35:21 -0600

    npm i, postgres with docker, .gits

---
+++ db.sql
@@ -0,0 +1,12 @@
+-- Docker can make the db
+-- CREATE DATABASE todo_list_db;
+
+--\c into todo_list_db
+
+CREATE TABLE todo_item (
+    todo_id SERIAL PRIMARY KEY,
+    description VARCHAR(255) NOT NULL
+);
+
+ALTER TABLE todo_item
+    ADD COLUMN important BOOLEAN DEFAULT FALSE;
