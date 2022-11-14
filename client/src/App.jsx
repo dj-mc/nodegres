07188338678e009e2120f2545503611529cdd26f
dj-mc <49037253+dj-mc@users.noreply.github.com>
2022-11-14 09:01:47 -0600

    Add table display to main app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
 import TodoInput from "./components/todo-input";
+import TodoList from "./components/todo-list";
 import "./App.css";
 
 function App() {
   return (
     <>
       <div className="App">
+        <h1>nodegres</h1>
+        <p>A todo list app built using the PERN stack</p>
         <TodoInput />
+        <TodoList />
       </div>
     </>
   );
